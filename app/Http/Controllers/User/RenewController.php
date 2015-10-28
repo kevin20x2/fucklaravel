cd4020044b61a02bad576396545ad60a0d7a7ed8
cxy <cxymrzero@gmail.com>
2015-10-28 19:52:29 +0800

    添加续借提示信息

--- app/Http/Controllers/User/RenewController.php
+++ app/Http/Controllers/User/RenewController.php
@@ -64,7 +64,7 @@ class RenewController extends Controller {
 
         }
 
-        return Redirect::to('/user/home');
+        return Redirect::to('/user/home')->with('message_success', '续借完成！');
     }
 
 }
